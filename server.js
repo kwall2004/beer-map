b7ec5e02cef85c3cf202c45fbfaf0d950165bb8d
Kevin Wall <kevin.wall@luxinteractive.com>
2019-07-17 14:37:16 -0400

    update server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/accounting-client'));
+app.use(express.static(__dirname + '/dist/beer-map'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/accounting-client/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/beer-map/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
